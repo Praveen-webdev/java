24402ce31c6da7a4a54f76b95c2b3af86251b8c3
github_Praveen-webdev <praveen1598810@gmail.com>
2022-01-21 18:17:46 +0530

    add InputCalculator

---
+++ InputCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class InputCalculator {
+    public static void main(String[] args) {
+        int sum = 0, n = 0, average;
+        Scanner scanner = new Scanner(System.in);
+        while (n >= 0) {
+            System.out.println("Enter #" + (n + 1) + " number");
+            boolean isValid = scanner.hasNextInt();
+            if (isValid) {
+                int input = scanner.nextInt();
+                sum += input;
+                n++;
+            } else
+                break;
+        }
+        average = sum / n;
+        System.out.println("\nSum is " + sum + "\nAverage is " + average);
+        scanner.close();
+    }
+}
