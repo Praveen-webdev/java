007c492f47129582b47e0aab65552dce7be5407f
github_Praveen-webdev <praveen1598810@gmail.com>
2022-01-22 22:45:55 +0530

    add patters

---
+++ problems/Patterns.java
@@ -0,0 +1,112 @@
+package problems;
+
+public class Patterns {
+    public static void main(String[] args) {
+        // * Inceasing triangle
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // * Decreasing traingle
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = i; j <= n; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // * Right increasing traingle
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        // for(int j=i;j<n;j++){
+        // System.out.print("- ");
+        // }
+        // for(int k=1;k<=i;k++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // * Right Decreasing traingle
+        // int n=6;
+        // for(int i=1;i<=n;i++){
+        // for(int j=i;j<=n;j++){
+        // System.out.print("* ");
+        // }
+        // for(int k=1;k<i;k++){
+        // System.out.print("- ");
+        // }
+        // System.out.println();
+        // }
+        // * Mountain
+        // int n=4;
+        // for(int i=1;i<=n;i++){
+        // //! Set-1:
+        // for(int j=i;j<n;j++){
+        // System.out.print(" ");
+        // }
+        // for(int j=1;j<=i;j++){
+        // System.out.print("* ");
+        // }
+        // for(int j=1;j<i;j++){
+        // System.out.print("* ");
+        // }
+        // //* Bringing empty space on right;
+        // for(int j=i;j<n;j++){
+        // System.out.print(" ");
+        // }
+        // //! Set-2:
+        // for(int j=i;j<n;j++){
+        // System.out.print(" ");
+        // }
+        // for(int j=1;j<=i;j++){
+        // System.out.print("* ");
+        // }
+        // for(int j=1;j<i;j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // //* Square
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        // for(int j=1;j<=n;j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // // * Diamond
+        // int n=4;
+        // // Upper portion
+        // for(int i=1;i<=n;i++){
+        // // Upper portion:
+        // for(int j=i;j<n;j++){
+        // System.out.print(" ");
+        // }
+        // for(int j=1;j<=i;j++){
+        // System.out.print("* ");
+        // }
+        // for(int j=1;j<i;j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // // Lower portion
+        // n-=1;
+        // for(int i=1;i<=n;i++){
+        // //Lower portion:
+        // for(int k=1;k<=i;k++){
+        // System.out.print(" ");
+        // }
+        // for(int k=i;k<=n;k++){
+        // System.out.print("* ");
+        // }
+        // for(int k=i;k<n;k++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+    }
+}
